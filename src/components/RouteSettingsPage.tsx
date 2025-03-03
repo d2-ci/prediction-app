93e21c10fee8bcab4f4085f93fb390a23d19bb85
Karim Bahgat <karimba@uio.no>
2025-03-03 11:53:31 +0000

    62dc625 chore: improve model selection layout

---
+++ src/components/RouteSettingsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TestRoute from './setup/TestRoute'
+import { Link } from 'react-router-dom'
+
+const RouteSettingsPage = () => {
+    return (
+        <>
+            <TestRoute />
+        </>
+    )
+}
+
+export default RouteSettingsPage
